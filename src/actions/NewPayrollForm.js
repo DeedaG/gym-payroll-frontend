ae2e1da7ba0e0e2a1656e48f4cc392a41a3d4476
Christia Tidwell <ChristiaTidwell@gmail.com>
2019-10-08 22:26:19 -0500

    created async actions for update, reset, and set payrolls

---
+++ src/actions/NewPayrollForm.js
@@ -0,0 +1,28 @@
+export const  updateNewPayrollForm = (name, value) => {
+  const formData = {name,value}
+
+  return {
+    type: "UPDATE_NEW_PAYROLL_FORM",
+    formData
+  }
+}
+
+export const  resetNewPayrollForm = () => {
+  return {
+    type: "RESET_NEW_PAYROLL_FORM"
+  }
+}
+
+export const setFormDataForEdit = payroll => {
+
+  const payrollFormData = {
+    payPeriod: payroll.attributes.payPeriod,
+    total: payroll.attributes.total,
+    groups: payroll.attributes.groups
+  }
+  console.log("payrollFormData:", payrollFormData)
+  return {
+    type: "SET_FORM_DATA_FOR_EDIT",
+    payrollFormData
+  }
+}
