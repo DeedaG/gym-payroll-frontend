33f95203edfa29401c91c154d13ad7107aa77646
Christia Tidwell <ChristiaTidwell@gmail.com>
2019-10-08 22:32:15 -0500

    added actions to payroll form and created reducers

--- src/components/newPayrollForm.js
+++ src/components/newPayrollForm.js
@@ -1,15 +1,15 @@
 import React from 'react'
-// import { updateNewPayrollForm } from '../actions/NewPayrollForm.js'
+import { updateNewPayrollForm } from '../actions/NewPayrollForm.js'
 import { connect } from 'react-redux'
 // import GroupCheckbox from './GroupCheckbox.js'
 
 
-const NewPayrollForm = ({ formData, updateNewPayrollForm, userId, handleSubmit }) => {
+const NewPayrollForm = ({ formData, updateNewPayrollForm, userId, handleSubmit, editMode }) => {
   const { payPeriod, total } = formData
 
   const handleChange = event => {
     const { name, value } = event.target
-    // updateNewPayrollForm(name,value)
+    updateNewPayrollForm(name,value)
   }
 
   return (
