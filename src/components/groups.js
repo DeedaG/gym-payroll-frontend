12573d6eaa0eaea23d3dff9146ff9fcb7913e2b0
Christia Tidwell <ChristiaTidwell@gmail.com>
2019-10-09 19:09:34 -0500

    created page for group detail

---
+++ src/components/groups.js
@@ -0,0 +1,29 @@
+import React from 'react'
+import { connect } from 'react-redux'
+import { Link } from 'react-router-dom'
+// import { addLoansToPool } from '../actions/loans.js'
+
+
+const Groups = ({groups}) => {
+
+  const groupCards =  groups.length > 0 ? groups.map(group =>
+
+    	<li key = {group.id}>
+          {group.attributes.name} - {group.attributes.hours}
+          - {group.attributes.inGym} - {group.attributes.offSite} - {group.attributes.team}
+        </li>) : null
+
+  return (
+    groupCards
+  )
+}
+
+  const mapStateToProps = state => {
+
+    return {
+      groups: state.groups,
+
+    }
+  }
+
+export default connect(mapStateToProps)(Groups)
