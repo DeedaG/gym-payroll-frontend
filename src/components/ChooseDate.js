d881ceaf5697280e10db3d7ca772b127b5ce5fea
Christia Tidwell <ChristiaTidwell@gmail.com>
2019-10-15 09:14:43 -0500

    added react calendar

---
+++ src/components/ChooseDate.js
@@ -0,0 +1,23 @@
+import React, { Component } from 'react';
+import Calendar from 'react-calendar';
+
+class ChooseDate extends Component {
+  state = {
+    workdate: new Date(),
+  }
+
+  onChange = workdate => this.setState({ workdate })
+
+  render() {
+    return (
+      <div>
+        <Calendar
+          onChange={this.onChange}
+          value={this.state.workdate}
+        />
+      </div>
+    );
+  }
+}
+
+export default ChooseDate;
