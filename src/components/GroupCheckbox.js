80992596371eea0052c12f726262f32f7b124732
Christia Tidwell <ChristiaTidwell@gmail.com>
2019-10-16 20:30:31 -0500

    checkbox will include calendar pop up

--- src/components/GroupCheckbox.js
+++ src/components/GroupCheckbox.js
@@ -1,11 +1,13 @@
 import React from 'react'
 import { updateNewPayrollForm } from '../actions/NewPayrollForm.js'
 import { connect } from 'react-redux'
+import Calendar from 'react-calendar';
 
 
 class GroupCheckbox extends React.Component {
   state = {
-    checkedGroups: []
+    checkedGroups: [],
+    workdate: new Date()
   }
 
   handleChangedGroups(e, value){
@@ -28,18 +30,25 @@ class GroupCheckbox extends React.Component {
 
  }
 
+ onChange = workdate => this.setState({ workdate })
+
   render() {
 // debugger
   return (
+
     <div >
-     {this.props.groups.map((group, index) =>
-       <li key = {group.id}> {group.attributes.name}
-       <><input
+      <Calendar
+        onChange={this.onChange}
+        value={this.state.workdate}
+      />
+      {this.props.groups.map((group, index) =>
+     <li key = {group.id}> {group.attributes.name}
+     <><input
        name="groups"
        type="checkbox"
        onClick={(e)=>this.handleChangedGroups(e,group)}
-      />{group.attributes.amount}<br></br></></li>)}
-      </div>
+    />{group.attributes.hours}<br></br></></li>)}
+    </div>
      )
     }
   }
