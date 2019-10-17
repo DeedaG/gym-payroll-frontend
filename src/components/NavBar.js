44964d668006293e50793fdf033790749ec1774b
Christia Tidwell <ChristiaTidwell@gmail.com>
2019-10-16 20:30:57 -0500

    added link for payroll list

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -7,6 +7,7 @@ import Logout from './Logout.js'
   const NavBar = ( { currentUser, loggedIn }) => {
     return (
       <div className="NavBar">
+        <NavLink exact activeClassName="active" to="/payrolls">My Payrolls   </NavLink>
         <NavLink exact activeClassName="active" to="/payrolls/new"><h2>Create Time Card</h2></NavLink>
         { loggedIn ? <Logout /> : null}
     </div>
