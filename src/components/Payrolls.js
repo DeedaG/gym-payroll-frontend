1f7ca801399a2282913fe8f0dcf057a3f2c7a0fa
Christia Tidwell <ChristiaTidwell@gmail.com>
2019-10-18 14:41:49 -0500

    rearrange view

--- src/components/Payrolls.js
+++ src/components/Payrolls.js
@@ -1,78 +1,29 @@
 import React from 'react'
 import { connect } from 'react-redux'
-
 import { Link } from 'react-router-dom'
 
-class Payrolls extends React.Component {
-  state = {
-    displaySorted: false,
-    inputDate: 0,
-    compareDate: 0
-   }
 
-   handleChange=(event)=> {
-     this.setState({
-       inputDate: event.target.value
-     })
-   }
+const Payrolls = ({payrolls}) => {
 
-   handleSubmit =(event) => {
-     event.preventDefault()
-      this.setState({
-        compareDate: this.state.inputDate,
-        inputDate: 0
-      })
-   }
+  const payrollCards =  payrolls.length > 0 ? payrolls.map(payroll =>
 
-    handleClick = () => {
-       this.setState({
-         displaySorted: !this.state.displaySorted
-      })
-    }
+    	<li key = {payroll.id}><><Link to ={`/payrolls`} key={payroll.attributes.id}>
+          {payroll.attributes.payPeriod},
+          {payroll.attributes.groups.map((group) => (
+            <li key={group.id}>{group.attributes.name}, {group.attributes.hours}</li> ))}
+        </Link><br></br><br></br></></li>) : null
 
-render() {
-// debugger
-      const allPayrollCards = this.props.payrolls.length > 0 ? this.props.payrolls.map(p =>
-        <li key = {p.id}><><Link to ={`/payrolls/${p.id}`} >{p.attributes.payPeriod}</Link>
-      <br></br><br></br></></li>) : null
-
-      const greaterPayrolls = this.props.payrolls.filter(p =>
-        p.attributes.payPeriod > this.state.compareDate
-      ).map(p =>
-        <><Link to ={`/payrolls/${p.id}`} >{p.attributes.payPeriod}</Link>
-      <br></br><br></br></>)
-
-
-      return (
-          <div>
-            <h3><label>Payrolls</label></h3>
-            <p>{this.state.compareDate > 0 && this.state.displaySorted ? greaterPayrolls
-                : allPayrollCards}</p>
-                <h4><label>Search Payrolls</label></h4>
-
-
-           <form onSubmit={this.handleSubmit}>
-
-             <input
-               placeholder="Search by Payroll Date"
-               onChange={this.handleChange}
-               value={this.state.inputDate}>
-             </input>
-             <input type="submit" />
-           </form>
-
-          </div>
-        )
-      }
-    }
-
+  return (
+    payrollCards
+  )
+}
 
   const mapStateToProps = state => {
+// debugger
     return {
-      payrolls: state.payrolls,
+      payrolls: state.payrolls
 
     }
   }
-
 
 export default connect(mapStateToProps)(Payrolls)
