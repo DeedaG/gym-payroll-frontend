e0a830609fc8627ad3ada43756326a8cb731612e
Christia Tidwell <ChristiaTidwell@gmail.com>
2019-10-22 09:21:50 -0500

    include new record link

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -7,9 +7,10 @@ import Logout from './Logout.js'
   const NavBar = ( { currentUser, loggedIn }) => {
     return (
       <div className="NavBar">
-        <NavLink exact activeClassName="active" to="/payrolls"><h3>My Payrolls</h3></NavLink>
-        <NavLink exact activeClassName="active" to="/payrolls/new"><h3>Create NewTime Card</h3></NavLink>
-        { loggedIn ? <Logout /> : null}
+        <NavLink exact activeClassName="active" to="/payrolls"><h4>Browse My Payrolls</h4></NavLink>
+        <NavLink exact activeClassName="active" to="/records/new"><h3>Create New Time Card</h3></NavLink>
+
+      { loggedIn ? <Logout /> : null}
     </div>
     )
 }
