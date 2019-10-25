bc7634c912b1625127272cc4547deec1b226a5b2
Christia Tidwell <ChristiaTidwell@gmail.com>
2019-10-25 08:44:40 -0500

    correcting names that didn't save earlier

--- src/components/newPayrollForm.js
+++ src/components/newPayrollForm.js
@@ -2,16 +2,10 @@ import React from 'react'
 import { updateNewPayrollForm } from '../actions/payrollForm.js'
 // import { createPayroll } from '../actions/payrolls.js';
 import { connect } from 'react-redux'
-import Workday from './Workday.js'
 import GroupCheckbox from './GroupCheckbox.js'
 
 
-<<<<<<< HEAD
-
-const NewPayrollForm = ({ formData, updateNewPayrollForm, userId, handleSubmit, editMode }) => {
-=======
 const NewPayrollForm = ({ formData, updateNewPayrollForm, handleSubmit, userId, editMode }) => {
->>>>>>> 171935c1e35a5ae988e64631118f4cb281562dd2
   const { payPeriod, total } = formData
 
   const handleChange = event => {
@@ -32,34 +26,29 @@ const NewPayrollForm = ({ formData, updateNewPayrollForm, handleSubmit, userId, 
         onChange={handleChange}
         value={payPeriod}
       /><br/>
-<<<<<<< HEAD
-        <Workday/>
+
         <GroupCheckbox/>
         <br/>
         <label>Total:</label>
-=======
+
       <GroupCheckbox />
       <br/>
->>>>>>> 171935c1e35a5ae988e64631118f4cb281562dd2
         <input
           placeholder="total"
           name="total"
           onChange={handleChange}
           value={total}
         /><br/>
-<<<<<<< HEAD
         <input
           type="submit"
           value={editMode ? "Update Payroll" : "Create Payroll"}
 
         /><br/>
 
-=======
           <input
             type="submit"
             value={editMode ? "Update Payroll" : "Create Payroll"}
           /><br/>
->>>>>>> 171935c1e35a5ae988e64631118f4cb281562dd2
       </form>
   )};
 
