2172dbc82968813c8ae76eb70ab5b31df344af98
Christia Tidwell <ChristiaTidwell@gmail.com>
2019-10-25 08:46:26 -0500

    deleted random code that should not be there

--- src/store.js
+++ src/store.js
@@ -1,11 +1,8 @@
 import currentUser from './reducers/currentUser.js'
 import loginForm from './reducers/loginForm.js'
 import signupForm from './reducers/signupForm.js'
-<<<<<<< HEAD
-import newPayrollForm from './reducers/newPayrollForm.js'
-=======
+import newPayrollForm from './reducers/payrollForm.js'
 import payrollForm from './reducers/payrollForm.js'
->>>>>>> 171935c1e35a5ae988e64631118f4cb281562dd2
 import payrolls from './reducers/payrolls.js'
 import groups from './reducers/groups.js'
 import { createStore, applyMiddleware, compose, combineReducers } from 'redux';
@@ -18,16 +15,13 @@ const reducer = combineReducers({
   currentUser,
   loginForm,
   signupForm,
-<<<<<<< HEAD
   newPayrollForm,
   groups,
-  payrolls
-
-=======
+  payrolls,
   payrollForm,
   groups,
   payrolls
->>>>>>> 171935c1e35a5ae988e64631118f4cb281562dd2
+
 })
 
 // const composeEnhancer = (typeof window !== 'undefined' && window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__) || compose;
