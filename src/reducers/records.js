fada1de054485c18765b14f25fcac8026d27e9a4
Christia Tidwell <ChristiaTidwell@gmail.com>
2019-11-01 12:32:58 -0500

    created matching reducer cases for record actions

---
+++ src/reducers/records.js
@@ -0,0 +1,20 @@
+const initialState = []
+
+export default (state = [], action) => {
+  switch(action.type) {
+    case "SET_MY_RECORDS":
+      return action.records
+    case "ADD_RECORD":
+      return state.concat(action.record)
+
+    case "UPDATE_RECORD":
+      return state.map(record => record.id === action.record.id ? action.record : record)
+    case "DELETE_RECORD":
+      return state.filter(record => record.id === action.recordId ? false : true)
+    case "CLEAR_MY_RECORDS":
+      return initialState
+
+    default:
+        return state
+    }
+  }
