06222ebe5dc44c49a97cb5aa6b4b6163a6826741
Christia Tidwell <ChristiaTidwell@gmail.com>
2019-11-01 12:32:22 -0500

    created reducer to match recordForm action

---
+++ src/reducers/recordForm.js
@@ -0,0 +1,22 @@
+const initialState = {
+    workdate: "",
+    totalHours: "",
+    groups: ""
+}
+
+export default (state=initialState, action) => {
+
+  switch (action.type) {
+    case 'UPDATE_RECORD_FORM':
+      return {
+        ...state,
+        [action.formData.name]: action.formData.value }
+    case 'RESET_RECORDL_FORM':
+      return initialState
+    case "SET_FORM_DATA_FOR_EDIT":
+        return action.payrollFormData
+
+    default:
+      return state
+  }
+}
