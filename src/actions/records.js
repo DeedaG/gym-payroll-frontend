07400793b7dc708df488816178a0a56e06c7b3b5
Christia Tidwell <ChristiaTidwell@gmail.com>
2019-11-01 12:31:32 -0500

    created to fetch records

---
+++ src/actions/records.js
@@ -0,0 +1,145 @@
+import { resetRecordForm } from './recordForm.js'
+import { updatePayroll } from './payrolls.js'
+
+
+export const setMyRecords = records => {
+  return {
+    type: "SET_MY_RECORDS",
+    records
+  }
+}
+
+
+export const clearMyRecords = () => {
+  return {
+    type: "CLEAR_MY_RECORDS"
+  }
+}
+
+export const addRecord = record => {
+  return {
+    type: 'ADD_RECORD',
+    record
+  }
+}
+
+
+export const deleteRecordSuccess = recordId => {
+  return {
+    type: 'DELETE_RECORD',
+    recordId
+  }
+}
+
+
+export const updateRecordSuccess = record => {
+  return {
+    type: 'UPDATE_RECORD',
+    record
+  }
+}
+
+// async actions
+
+export const getMyRecords = () => {
+  return dispatch => {
+    return fetch("http://localhost:3000/api/v1/records",
+    {
+      credentials: "include",
+      method: 'GET',
+      headers: {
+        "Content-Type": "application/json"
+      },
+    })
+      .then(r => r.json())
+      .then(response => {
+        if (response.error) {
+          alert(response.error)
+        } else {
+
+          dispatch(setMyRecords(response.data))
+          dispatch(setMyRecords(response.data))
+        }
+      })
+      .catch(console.log)
+  }
+}
+
+export const createRecord = ( recordData, history ) => {
+  return dispatch => {
+    const sendableRecordData = {
+        id: recordData.id,
+        workdate: recordData.workdate,
+        totalHours: recordData.totalHours,
+        groups: recordData.groups
+    }
+    return fetch(`http://localhost:3000/api/v1/payrolls/${sendableRecordData.payroll_id}/records`, {
+      credentials: 'include',
+      method: 'POST',
+      headers: {
+        "content-Type": "application/json"
+      },
+      body: JSON.stringify(sendableRecordData)
+    })
+    .then(r => r.json())
+    .then(resp => {
+      if (resp.error) {
+        alert(resp.error)
+      }else{
+      dispatch(addRecord(resp.data))
+      dispatch(resetRecordForm())
+      history.push(`records/${resp.data.id}`)
+     }
+    })
+  }
+}
+
+export const updateRecord = ( recordData, history ) => {
+  return dispatch => {
+    console.log(recordData)
+    const sendableRecordData = {
+        workdate: recordData.workdate,
+        totalHours: recordData.totalHours,
+        groups: recordData.groups
+    }
+    return fetch(`http://localhost:3000/api/v1/records/${recordData.recordId}`, {
+      credentials: 'include',
+      method: 'PATCH',
+      headers: {
+        "content-Type": "application/json"
+      },
+      body: JSON.stringify(sendableRecordData)
+    })
+    .then(r => r.json())
+    .then(resp => {
+      if (resp.error) {
+        alert(resp.error)
+      }else {
+      dispatch(updateRecordSuccess(resp.data))
+      history.push(`/records/${resp.data.id}`)
+     }
+    })
+  }
+}
+
+export const deleteRecord = (recordId, history) => {
+  return dispatch => {
+    return fetch(`http://localhost:3000/api/v1/records/${recordId}`, {
+      credentials: "include",
+      method: "DELETE",
+      headers: {
+        "Content-Type": "application/json"
+      }
+    })
+      .then(r => r.json())
+      .then(resp => {
+        if (resp.error) {
+          alert(resp.error)
+        } else {
+          dispatch(deleteRecordSuccess(recordId))
+          history.push(`/records`)
+        }
+      })
+      .catch(console.log)
+  }
+}
