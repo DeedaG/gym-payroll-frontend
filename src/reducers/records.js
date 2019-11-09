9652f85a7f093de20cb7086bf53840c0d3c12e28
Christia Tidwell <ChristiaTidwell@gmail.com>
2019-11-08 18:53:44 -0600

    removed total from new payroll form for now

--- src/reducers/records.js
+++ src/reducers/records.js
@@ -5,8 +5,8 @@ export default (state = [], action) => {
     case "SET_MY_RECORDS":
       return action.records
     case "ADD_RECORD":
+    // debugger
       return state.concat(action.record)
-
     case "UPDATE_RECORD":
       return state.map(record => record.id === action.record.id ? action.record : record)
     case "DELETE_RECORD":
