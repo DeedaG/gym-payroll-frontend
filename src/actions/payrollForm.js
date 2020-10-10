9c3147a1f07b047bf1165f6b3a28d4bf934c072c
Christia <ChristiaTidwell@gmail.com>
2020-10-09 19:49:46 -0500

    corrected definition for payroll id

--- src/actions/payrollForm.js
+++ src/actions/payrollForm.js
@@ -7,18 +7,41 @@ export const  updateNewPayrollForm = (name, value) => {
   }
 }
 
+export const  updatePayrollForm = (name, value) => {
+  const formData = {name,value}
+
+  return {
+    type: "UPDATE_PAYROLL_FORM",
+    formData
+  }
+}
+
 export const  resetNewPayrollForm = () => {
   return {
     type: "RESET_NEW_PAYROLL_FORM"
   }
 }
 
+export const  resetPayrollForm = () => {
+  return {
+    type: "RESET_PAYROLL_FORM"
+  }
+}
+
 export const setFormDataForEdit = (payroll) => {
+  const myFunc = (total, num) => {
+    return total + num;
+  }
+
+  const calcTotal =
+  payroll.attributes.records.map(
+    record =>
+    parseInt(record.totalHours)).reduce(myFunc, 0)
 
   const payrollFormData = {
     id: payroll.id,
     payPeriod: payroll.attributes.payPeriod,
-    total: payroll.attributes.total,
+    total: calcTotal,
     records: payroll.attributes.records
   }
   console.log("payrollFormData:", payrollFormData)
